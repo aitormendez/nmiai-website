70229d4f8ed1de890875980a9d3dd8fbc8bdbfec
aitor <aitor@e451.net>
2022-11-08 12:00:54 +0100

    Post block: mobile CSS

--- site/web/app/themes/sage/resources/views/blocks/post-block.blade.php
+++ site/web/app/themes/sage/resources/views/blocks/post-block.blade.php
@@ -6,9 +6,9 @@
       sizes="100%">
   @endif
 
-  <div class="my-6 flex items-center justify-start">
-    <span class="start-text mr-4 font-serif">{{ $post_block['start_text'] }}</span>
-    <div class="progress relative mr-4 flex w-52 items-center justify-between">
+  <div class="my-6 flex flex-wrap items-center justify-between md:justify-start">
+    <span class="start-text order-1 mr-4 font-serif md:order-none">{{ $post_block['start_text'] }}</span>
+    <div class="progress relative mb-2 flex w-full items-center justify-between md:mr-4 md:mb-0 md:w-52">
       <div class="progress-bar absolute h-0 w-0 border"></div>
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
         <circle id="Elipse_2596" data-name="Elipse 2596" cx="8" cy="8" r="8"
@@ -19,7 +19,7 @@
           class="fill-dark" />
       </svg>
     </div>
-    <span class="end-text font-serif italic">{{ $post_block['end_text'] }}</span>
+    <span class="end-text order-1 font-serif italic md:order-none">{{ $post_block['end_text'] }}</span>
   </div>
 
   <div class="my-6 text-2xl">{!! $post_block['post_excerpt'] !!}</div>
