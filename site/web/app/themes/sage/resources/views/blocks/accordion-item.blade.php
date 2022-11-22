22d105e79377cc2e1a4465a025d34bd1950a7c6d
aitor <aitor@e451.net>
2022-11-22 10:51:05 +0100

    Accordion item block

--- site/web/app/themes/sage/resources/views/blocks/accordion-item.blade.php
+++ site/web/app/themes/sage/resources/views/blocks/accordion-item.blade.php
@@ -1,7 +1,11 @@
 <li class="accordion-group {{ $block->classes }} mb-6 p-6 transition-colors duration-1000 last:pb-0 dark:text-white">
-  <div class="accordion-menu cursor-pointer">
-    <h2 class="text-3xl md:text-4xl lg:text-5xl">{{ $accordion_item }}</h2>
-    <button>+</button>
+  <div class="accordion-menu cursor-pointer select-none">
+    <h2 class="text-4xl uppercase md:text-5xl lg:text-6xl">{{ $accordion_item }}</h2>
+    <div class="my-4 w-10">
+      <svg viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M13 16V29L16 29V16H29V13H16V0H13V13H0V16H13Z" class="fill-dark dark:fill-white" />
+      </svg>
+    </div>
   </div>
   <div class="accordion-content h-0 overflow-hidden">
     <InnerBlocks />
--- site/web/app/themes/sage/resources/views/blocks/preview-section.blade.php
+++ site/web/app/themes/sage/resources/views/blocks/preview-section.blade.php
@@ -2,7 +2,7 @@
   <div class="bottom my-6 flex flex-wrap items-center justify-between md:justify-start">
 
     <div class="progress relative mb-4 flex w-full items-center justify-between">
-      <div class="progress-bar absolute h-0 w-full border"></div>
+      <div class="progress-bar text-dark absolute h-0 w-full border"></div>
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
         <circle id="Elipse_2596" data-name="Elipse 2596" cx="8" cy="8" r="8" class="fill-dark" />
       </svg>
