6ed9a47ca44cb938e91d3f42c5821e068d7f5bde
aitor <aitor@e451.net>
2022-11-27 17:13:45 +0100

    Nav fix

--- site/web/app/themes/sage/resources/views/sections/header.blade.php
+++ site/web/app/themes/sage/resources/views/sections/header.blade.php
@@ -1,14 +1,14 @@
 <header id="banner"
   class="banner text-dark fixed flex w-full justify-between bg-gradient-to-b from-white to-transparent pt-3 pb-10 transition-all duration-500">
 
-  @include('partials.navigation')
-
   <a class="brand relative ml-6 text-lg uppercase" href="{{ home_url('/') }}">
     <span class="mr-3 align-middle">No</span><span class="align-middle text-2xl">{{ $emoji['header_emoji'] }}</span><span
       class="ml-3 align-middle">Is An Island</span>
   </a>
 
 </header>
+
+@include('partials.navigation')
 
 <div id="tagline" class="text-dark dark:text-light fixed top-3 hidden font-serif font-bold md:block">
   {{ get_bloginfo('description') }}
