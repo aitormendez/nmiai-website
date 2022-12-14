ee459276e651b088112cab4e9bc8314c9d2c836a
aitor <aitor@e451.net>
2022-12-14 14:30:18 +0100

    Page header: fix to column

--- site/web/app/themes/sage/resources/views/partials/page-header.blade.php
+++ site/web/app/themes/sage/resources/views/partials/page-header.blade.php
@@ -21,7 +21,7 @@
     </h1>
   </div>
 @else
-  <div class="page-header not-prose flex justify-center px-6 py-20 text-center">
+  <div class="page-header not-prose is-style-column flex justify-center px-6 py-20 text-center">
     <h1 class="text-4xl font-bold uppercase md:text-5xl lg:text-7xl xl:text-8xl">
       @if ($header_html)
         {!! $header_html !!}
