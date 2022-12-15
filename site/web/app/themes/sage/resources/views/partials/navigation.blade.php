8600feaa663e5c3f5b912f32d9c90fb914a0a9ca
aitor <aitor@e451.net>
2022-12-15 09:53:42 +0100

    Header nav: resize

--- site/web/app/themes/sage/resources/views/partials/navigation.blade.php
+++ site/web/app/themes/sage/resources/views/partials/navigation.blade.php
@@ -1,6 +1,6 @@
 @if ($navigation)
   <ul id="main-menu"
-    class="my-menu bg-dark xl:text-10xl fixed top-0 z-40 h-screen w-screen pt-24 text-7xl text-white md:text-8xl lg:text-9xl"
+    class="my-menu bg-dark fixed top-0 z-40 h-screen w-screen pt-24 text-6xl text-white md:text-7xl lg:text-8xl xl:text-9xl"
     style="clip-path: circle(0px at calc(100vw - 3.75rem) 2.25rem); ">
 
     @foreach ($navigation as $item)
