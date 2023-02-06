8740bccd1504e32e558e4a5ea47f9dfa69a75491
aitor <aitor@e451.net>
2023-02-06 10:48:55 +0100

    People block: trello fixes

--- site/web/app/themes/sage/resources/scripts/people.js
+++ site/web/app/themes/sage/resources/scripts/people.js
@@ -7,12 +7,25 @@ export function people() {
   peopleBlocks.forEach(function (block) {
     let peopleText = block.querySelector('.text').innerHTML;
     let peopleName = block.querySelector('.name').innerHTML;
+    let peopleTitle = block.querySelector('.title').innerHTML;
+    let peopleEmail = block.querySelector('.email').innerHTML;
 
     block.addEventListener('click', () => {
       mainFlap.classList.remove('hidden');
       mainFlap.classList.add('flex');
       mainFlapContent.innerHTML =
-        '<p class="text-3xl md:text-6xl my-20">' + peopleName + '</p><div>' + peopleText + '</div>';
+        '<div class="text-[46px] md:text-6xl mt-20 mb-4 leading-tight text-light">' +
+        peopleName +
+        '</div>' +
+        '<div class="font-serif text-light text-xl italic">' +
+        peopleTitle +
+        '</div>' +
+        '<div class="font-serif">' +
+        peopleText +
+        '</div>' +
+        '<div class="text-light font-thin">' +
+        peopleEmail +
+        '</div>';
       setTimeout(function () {
         mainFlap.classList.add('opacity-100');
         mainFlap.classList.remove('opacity-0');
