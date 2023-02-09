5846624680e8534cbe923b319481f934847b56a6
aitor <aitor@e451.net>
2023-02-09 15:02:58 +0100

    Post Block: only the image is a link

--- site/web/app/themes/sage/resources/views/blocks/post-block.blade.php
+++ site/web/app/themes/sage/resources/views/blocks/post-block.blade.php
@@ -1,29 +1,29 @@
 <article class="{{ $block->classes }} not-prose block py-14 md:py-20" role="article">
-  <a href="{{ $post_block['permalink'] }}">
-    @if (array_key_exists('post_thumbnail_data', $post_block))
+  @if (array_key_exists('post_thumbnail_data', $post_block))
+    <a href="{{ $post_block['permalink'] }}">
       <img class="" src="{{ $post_block['post_thumbnail_data']['src'][0] }}"
         alt="{{ $post_block['post_thumbnail_data']['alt'] }}" srcset="{{ $post_block['post_thumbnail_data']['srcset'] }}"
         sizes="100%">
-    @endif
+    </a>
+  @endif
 
-    <div class="bottom my-6 flex flex-wrap items-center justify-between md:justify-start">
-      <span class="start-text order-1 mr-4 font-serif font-bold md:order-none">{{ $post_block['start_text'] }}</span>
-      <div class="progress relative mb-2 flex w-full items-center justify-between md:mr-4 md:mb-0 md:w-52">
-        <div class="progress-bar text-dark dark:text-light absolute h-0 w-0 border-t"></div>
-        <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 16 16">
-          <circle id="Elipse_2596" data-name="Elipse 2596" cx="8" cy="8" r="8"
-            class="fill-dark" />
-        </svg>
-        <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 16 16">
-          <circle id="Elipse_2596" data-name="Elipse 2596" cx="8" cy="8" r="8"
-            class="fill-dark" />
-        </svg>
-      </div>
-      <span class="end-text order-1 font-serif font-bold italic md:order-none">{{ $post_block['end_text'] }}</span>
+  <div class="bottom my-6 flex flex-wrap items-center justify-between md:justify-start">
+    <span class="start-text order-1 mr-4 font-serif font-bold md:order-none">{{ $post_block['start_text'] }}</span>
+    <div class="progress relative mb-2 flex w-full items-center justify-between md:mr-4 md:mb-0 md:w-52">
+      <div class="progress-bar text-dark dark:text-light absolute h-0 w-0 border-t"></div>
+      <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 16 16">
+        <circle id="Elipse_2596" data-name="Elipse 2596" cx="8" cy="8" r="8"
+          class="fill-dark" />
+      </svg>
+      <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 16 16">
+        <circle id="Elipse_2596" data-name="Elipse 2596" cx="8" cy="8" r="8"
+          class="fill-dark" />
+      </svg>
     </div>
+    <span class="end-text order-1 font-serif font-bold italic md:order-none">{{ $post_block['end_text'] }}</span>
+  </div>
 
-    @if ($post_block['post_excerpt'])
-      <div class="fade my-6 text-2xl">{!! $post_block['post_excerpt'] !!}</div>
-    @endif
-  </a>
+  @if ($post_block['post_excerpt'])
+    <div class="fade my-6 text-2xl">{!! $post_block['post_excerpt'] !!}</div>
+  @endif
 </article>
