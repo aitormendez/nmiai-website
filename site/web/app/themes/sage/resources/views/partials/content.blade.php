50fae427acabd8af6323f8dab5676000e89ac0c0
aitor <aitor@e451.net>
2023-02-16 08:02:02 +0100

    Work page: Restrict link to image

--- site/web/app/themes/sage/resources/views/partials/content.blade.php
+++ site/web/app/themes/sage/resources/views/partials/content.blade.php
@@ -2,21 +2,23 @@
   $thumb = $featured_img();
   global $counter;
 @endphp
-<a role="article" href="{{ get_permalink() }}" {{ post_class('md:w-1/2 md:odd:pr-2 md:even:pl-2 mb-20 no-underline') }}>
+<article {{ post_class('md:w-1/2 md:odd:pr-2 md:even:pl-2 mb-20 no-underline') }}>
   <header>
     @if (array_key_exists('img', $thumb))
-      @if ($thumb['img']['mime'] === 'image/gif')
-        <img src="{{ $thumb['img']['url'] }}" alt="{{ $thumb['img']['alt'] }}">
-      @else
-        <img src="{{ $thumb['img']['url'] }}" alt="{{ $thumb['img']['alt'] }}" srcset="{{ $thumb['img']['srcset'] }}"
-          sizes="(max-width: 768px) 100vw, 50vw">
-      @endif
+      <a href="{{ get_permalink() }}">
+        @if ($thumb['img']['mime'] === 'image/gif')
+          <img src="{{ $thumb['img']['url'] }}" alt="{{ $thumb['img']['alt'] }}">
+        @else
+          <img src="{{ $thumb['img']['url'] }}" alt="{{ $thumb['img']['alt'] }}" srcset="{{ $thumb['img']['srcset'] }}"
+            sizes="(max-width: 768px) 100vw, 50vw">
+        @endif
+      </a>
     @endif
 
     <div class="bottom my-6 flex flex-wrap items-center justify-between md:justify-start">
       <span
         class="start-text order-1 mr-4 font-serif font-bold md:order-none">{{ substr(str_repeat(0, 2) . $counter, -2) }}</span>
-      <div class="progress relative mb-2 flex w-full items-center justify-between md:mr-4 md:mb-0 md:w-52">
+      <div class="progress text-dark relative mb-2 flex w-full items-center justify-between md:mr-4 md:mb-0 md:w-52">
         <div class="progress-bar absolute h-0 w-0 border-t"></div>
         <svg class="h-2 w-2 md:h-[11px] md:w-[11px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
           <circle id="Elipse_2596" data-name="Elipse 2596" cx="8" cy="8" r="8"
@@ -35,5 +37,5 @@
       <p class="terms hidden font-serif md:block">{!! $thumb['terms'] !!}</p>
     @endif
   </header>
-</a>
+</article>
 @php $counter++; @endphp
