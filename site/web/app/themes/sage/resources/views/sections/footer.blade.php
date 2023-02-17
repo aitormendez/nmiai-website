0e92cacfc7c5a09dac9fddfc6f299938b2553fa8
aitor <aitor@e451.net>
2023-02-17 18:22:55 +0100

    Footer mobile fixes

--- site/web/app/themes/sage/resources/views/sections/footer.blade.php
+++ site/web/app/themes/sage/resources/views/sections/footer.blade.php
@@ -1,16 +1,16 @@
 <footer class="content-info bg-dark p-6 text-white">
-  <div class="down grid gap-x-2 text-xs md:gap-x-4"">
+  <div class="down grid gap-4 gap-x-2 text-xs"">
     <div class="left-text flex items-center">
       @option('footer_left_text')
     </div>
-    <div class="right-text flex items-center justify-end">
+    <div class="right-text flex items-center md:justify-end">
       <span>
         @option('footer_right_text')
       </span>
     </div>
 
     @hasoptions('footer_links')
-      <ul class="social mb-4 flex w-full gap-4 md:mb-0 md:w-auto">
+      <ul class="social flex w-full gap-4 md:mb-0 md:w-auto">
         @options('footer_links')
           <li>
             <a href="@sub('footer_link', 'url')" target="@sub('footer_link', 'target')">
