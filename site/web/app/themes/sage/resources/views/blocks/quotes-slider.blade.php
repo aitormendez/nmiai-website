9ff2a0bc288309ee1f0c5b66750cd005caa752a3
aitor <aitor@e451.net>
2023-04-20 09:10:25 +0200

    H2 font size

--- site/web/app/themes/sage/resources/views/blocks/quotes-slider.blade.php
+++ site/web/app/themes/sage/resources/views/blocks/quotes-slider.blade.php
@@ -1,14 +1,14 @@
 @if ($quotes)
   <div class="{{ $block->classes }}">
     <div class="wrapper" style="{{ $padding }}">
-      <div class="swiper-quotes swiper w-full !overflow-visible text-3xl md:text-5xl">
+      <div class="swiper-quotes swiper leading-h2 2xl:text-7con5xl w-full !overflow-visible text-6xl">
         <div class="swiper-wrapper">
           @foreach ($quotes['quotes'] as $quote)
             <div class="swiper-slide flex flex-col justify-between">
               <div class="quote flex py-6">
                 <span class="dark:text-white">“</span><span class="dark:text-white">{!! $quote['quotes_slider_quote'] !!}”</span>
               </div>
-              <div class="data ml-6 font-serif text-2xl">
+              <div class="data ml-8 font-serif text-2xl md:ml-10">
                 <div class="name font-rigid dark:text-white">
                   {!! $quote['quotes_slider_name'] !!}
                 </div>
