4a9e6b6c1e5b6f5b630b2c615b68d6c4e050194f
aitor <aitor@e451.net>
2023-05-18 12:25:58 +0200

    Style fixes quotes block

--- site/web/app/themes/sage/resources/views/blocks/quotes-slider.blade.php
+++ site/web/app/themes/sage/resources/views/blocks/quotes-slider.blade.php
@@ -1,18 +1,18 @@
 @if ($quotes)
   <div class="{{ $block->classes }}">
     <div class="wrapper" style="{{ $padding }}">
-      <div class="swiper-quotes swiper leading-h2 2xl:text-7con5xl w-full !overflow-visible text-6xl">
+      <div class="swiper-quotes swiper leading-h2 2xl:text-7con5xl w-full !overflow-visible text-2xl md:text-6xl">
         <div class="swiper-wrapper">
           @foreach ($quotes['quotes'] as $quote)
             <div class="swiper-slide flex flex-col justify-between">
               <div class="quote flex py-6">
                 <span class="dark:text-white">“</span><span class="dark:text-white">{!! $quote['quotes_slider_quote'] !!}”</span>
               </div>
-              <div class="data ml-8 font-serif text-2xl md:ml-10">
+              <div class="data ml-3 font-serif text-sm md:ml-10 md:text-2xl">
                 <div class="name font-rigid dark:text-white">
                   {!! $quote['quotes_slider_name'] !!}
                 </div>
-                <div class="text-middle text-lg italic">{!! $quote['quotes_slider_subtitle'] !!}</div>
+                <div class="text-middle italic">{!! $quote['quotes_slider_subtitle'] !!}</div>
               </div>
             </div>
           @endforeach
