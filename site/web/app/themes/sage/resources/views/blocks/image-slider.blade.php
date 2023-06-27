f04408de54e4eb84ccdcea28622a57c1155e4a64
aitor <aitor@e451.net>
2023-06-27 15:13:40 +0200

    fix slider overflow

--- site/web/app/themes/sage/resources/views/blocks/image-slider.blade.php
+++ site/web/app/themes/sage/resources/views/blocks/image-slider.blade.php
@@ -1,7 +1,7 @@
 @if ($gallery)
   <div class="{{ $block->classes }}">
     <div class="wrapper" style="{{ $padding }}">
-      <div class="swiper-images swiper w-full !overflow-visible text-3xl md:text-5xl">
+      <div class="swiper-images swiper w-full overflow-hidden text-3xl md:text-5xl">
         <div class="swiper-wrapper">
           @foreach ($gallery as $img)
             <div class="swiper-slide flex flex-col justify-between">
