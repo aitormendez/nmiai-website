e1589f50ca134d70399ec7d2e4ce8eb944934c1c
aitor <aitor@e451.net>
2023-06-27 10:22:12 +0200

    fix wpml registration

--- site/web/app/themes/sage/app/filters.php
+++ site/web/app/themes/sage/app/filters.php
@@ -14,3 +14,12 @@ namespace App;
 add_filter('excerpt_more', function () {
     return sprintf(' &hellip; <a href="%s">%s</a>', get_permalink(), __('Continued', 'sage'));
 });
+
+
+// https://wpml.org/es/forums/topic/no-puedo-registrar-el-sitio-2/
+
+add_action('shutdown', function () {
+    if (function_exists('icl_enable_capabilities')) {
+        icl_enable_capabilities();
+    }
+});
