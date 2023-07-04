50f5584975545809efe03dd36413c6d75f228b50
aitor <aitor@e451.net>
2023-07-04 17:04:07 +0200

    before-after mouse block. Fix firefox aspect rato

--- site/web/app/themes/sage/resources/views/blocks/before-after-mouse.blade.php
+++ site/web/app/themes/sage/resources/views/blocks/before-after-mouse.blade.php
@@ -1,9 +1,10 @@
 <section class="{{ $block->classes }}">
   <div class="not-prose relative overflow-hidden" style="{{ $padding }}">
-    <img src="{{ $beforeafter['image_before']['url'] }}" alt="{{ $beforeafter['image_before']['alt'] }}"
-      srcset="{{ $beforeafter['image_before']['srcset'] }}" sizes="{{ $beforeafter['image_before']['sizes'] }}">
+    <img class="max-w-auto w-full" src="{{ $beforeafter['image_before']['url'] }}"
+      alt="{{ $beforeafter['image_before']['alt'] }}" srcset="{{ $beforeafter['image_before']['srcset'] }}"
+      sizes="{{ $beforeafter['image_before']['sizes'] }}">
 
-    <img class="img-after absolute top-0 left-0" src="{{ $beforeafter['image_after']['url'] }}"
+    <img class="img-after max-w-auto absolute top-0 left-0 w-full" src="{{ $beforeafter['image_after']['url'] }}"
       alt="{{ $beforeafter['image_after']['alt'] }}" srcset="{{ $beforeafter['image_after']['srcset'] }}"
       sizes="{{ $beforeafter['image_after']['sizes'] }}" style="clip-path: polygon(0% 0, 0% 0%, 0% 100%, 0 100%);">
 
